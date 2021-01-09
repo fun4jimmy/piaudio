abfc492fcc3fee1771d623a572963163efe96b11
James Whitworth <fun4jimmy@gmail.com>
2021-01-09 16:27:20 +0000

    Revert "Add a class method for converting a string value to an instance of the ServiceState enum. This is useful for converting from a REST api value to the Python type."
    This reverts commit 2177322503ff01c7057f99099d2094f1b53a2433.

--- piaudio/blueprints/__init__.py
+++
@@ -1,5 +0,0 @@
-"""The blueprints subpackage defines all the flask application blueprints.
-
-Using blueprints allows the code to be much more modular.
-"""
-from piaudio.blueprints.index import index
