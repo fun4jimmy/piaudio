f2fee2bb1178235dfdee741c742c5362931fb03c
James Whitworth <fun4jimmy@gmail.com>
2021-01-09 16:34:14 +0000

    Add a very simple Flask/Connexions application with an index page that calls our api to set the plexamp or alsaloop services to active. The index page is defined as a blueprint to allow the route definition to be in a separate module to keep the package init function clean and avoid some pylint errors about unused variables (caused by defining the route funciton inline but never calling it).

---
+++ piaudio/blueprints/__init__.py
@@ -0,0 +1,5 @@
+"""The blueprints subpackage defines all the flask application blueprints.
+
+Using blueprints allows the code to be much more modular.
+"""
+from piaudio.blueprints.index import index
