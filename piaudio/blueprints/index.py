50ee08a69b5dc779fa6f9f53e1451509f43539d6
James Whitworth <fun4jimmy@gmail.com>
2021-01-10 02:09:25 +0000

    Dynamically populate the available services as radio inputs using a template and hook them up to the javascript frontend api.

--- piaudio/blueprints/index.py
+++ piaudio/blueprints/index.py
@@ -2,10 +2,13 @@
 from flask import Blueprint
 from flask import render_template
 
+from piaudio import api
+
 index = Blueprint('index', __name__)
 
 
 @index.route('/')
 def render():
     """Render the index page static template."""
-    return render_template('index.html')
+    services = api.list_services()
+    return render_template('index.html', services=services)
