26ee1a401b93b2f700069b97532d9e493293b72a
James Whitworth <fun4jimmy@gmail.com>
2021-01-11 00:13:33 +0000

    Add Flask-Assets to add support for sass and to allow bundling of assets together in a compressed single file.

--- piaudio/__init__.py
+++ piaudio/__init__.py
@@ -1,5 +1,9 @@
 """Flask site application factory for switching between audio applications on the host system."""
+import os.path
+
 from flask import render_template
+from flask_assets import Environment
+from flask_assets import Bundle
 
 import connexion
 
@@ -21,6 +25,17 @@ def make_app():
     api.add_service(PlexampService())
 
     flask_application = application.app
+
+    asset_environment = Environment(flask_application)
+
+    javascript_bundle = Bundle('javascript/api.js', filters='jsmin', output='bundles/packed.js')
+    asset_environment.register('javascript_bundle', javascript_bundle)
+
+    asset_environment.config['LIBSASS_STYLE'] = 'compressed'
+
+    css_bundle = Bundle('sass/main.scss', filters='libsass', output='bundles/packed.css')
+    asset_environment.register('css_bundle', css_bundle)
+
     # we use blueprints, rather than define the routes within this function, to keep the code more modular
     flask_application.register_blueprint(index)
 
