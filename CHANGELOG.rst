61ceea67c3b243f030da3666cb30622f25c12893
James Whitworth <fun4jimmy@gmail.com>
2021-01-14 00:31:14 +0000

    Update setup.py to fail with a helpful message if the wrong version of setuptools is found when installing.

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,6 +1,10 @@
 Changelog (piaudio)
 ===================
 
+0.0.2 (2021-01-14)
+
+- Update setup.py to fail with a helpful message if the wrong version of setuptools is found when installing.
+
 0.0.1 (2021-01-13)
 ------------------
 
--- setup.py
+++ setup.py
@@ -1,3 +1,17 @@
+"""Stub setup.py as pip does not yet support installing directly from a setup.cfg file."""
+import sys
+
+from distutils.version import StrictVersion
+
+from setuptools import __version__ as setuptools_version
 from setuptools import setup
+
+# The way we handle reading the version number from __init__.py without requiring
+# any package imports to be installed requires a certain version of setuptools.
+MINIMUM_SETUPTOOLS_VERSION = '46.4.0'
+
+if StrictVersion(setuptools_version) < StrictVersion(MINIMUM_SETUPTOOLS_VERSION):
+    print('Expected setuptools version {} or higher, got version {}.'.format(MINIMUM_SETUPTOOLS_VERSION, setuptools_version))
+    sys.exit(1)
 
 setup()
